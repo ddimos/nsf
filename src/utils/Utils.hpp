997ebe886fdc3f24c08879fa25fccfe52ae802c7
ddimos <ddimasul@gmail.com>
2025-04-28 22:46:08 +0200

    Fix clang compilation issue

--- src/utils/Utils.hpp
+++ src/utils/Utils.hpp
@@ -10,8 +10,8 @@ namespace nsf
 
 inline int sequenceGreaterThan(SequenceNumber _s1, SequenceNumber _s2)
 {
-    constexpr SequenceNumber HALF_MAX_SEQUENCE_NUMBER = std::ceil(MAX_SEQUENCE_NUMBER / 2.f);
-    
+    static const SequenceNumber HALF_MAX_SEQUENCE_NUMBER = static_cast<SequenceNumber>(std::ceil(MAX_SEQUENCE_NUMBER / 2.f));
+
     return (( _s1 > _s2 ) && ( _s1 - _s2 <= HALF_MAX_SEQUENCE_NUMBER )) || 
            (( _s1 < _s2 ) && ( _s2 - _s1  > HALF_MAX_SEQUENCE_NUMBER ));
 }
